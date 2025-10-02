7ac4d78768d4985723446601bb55e6b00b22f03e
YH <13736616102@qq.com>
2025-10-02 15:48:44 +0800

    v0.0

---
+++ src/main/java/sg/nusiss/t6/caproject/CaProjectApplication.java
@@ -0,0 +1,13 @@
+package sg.nusiss.t6.caproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sg/nusiss/t6/caproject/CaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nusiss.t6.caproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
