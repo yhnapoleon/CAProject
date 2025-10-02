2fe3d6743af133bf590d06d684ab740db4e3c026
ZHAO-1221 <zhaojiayi_2025@163.com>
2025-10-02 16:59:12 +0800

    测试

--- src/main/java/sg/nusiss/t6/caproject/fuck.java
+++ src/main/java/sg/nusiss/t6/caproject/fuck.java
@@ -3,10 +3,9 @@ package sg.nusiss.t6.caproject;
 public class fuck {
     public static void fuck(String name,String student){
         System.out.println(student + " Fuck " +name);
-
     }
 
     public static void main(String[] args) {
-        fuck("Tin","YK");
+        fuck("Sun","YK");
     }
 }
