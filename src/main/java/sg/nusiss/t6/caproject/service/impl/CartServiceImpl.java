abfa29a4d80861c41e46e4d33a5fa2b52154620c
ZHAO-1221 <zhaojiayi_2025@163.com>
2025-10-05 18:20:39 +0800

    更新架构

---
+++ src/main/java/sg/nusiss/t6/caproject/service/impl/CartServiceImpl.java
@@ -0,0 +1,6 @@
+package sg.nusiss.t6.caproject.service.impl;
+
+import sg.nusiss.t6.caproject.service.CartService;
+
+public class CartServiceImpl implements CartService {
+}
---
+++ src/main/java/sg/nusiss/t6/caproject/service/impl/OrderServiceImpl.java
@@ -0,0 +1,6 @@
+package sg.nusiss.t6.caproject.service.impl;
+
+import sg.nusiss.t6.caproject.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+}
